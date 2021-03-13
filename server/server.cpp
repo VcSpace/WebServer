b2c885e1683bb9bd0cd3a656f139c845fea0b40c
Vcvc <vcvckw@gmail.com>
2021-03-13 18:39:39 +0800

    server

--- server/server.cpp
+++ server/server.cpp
@@ -1,5 +1,22 @@
-//
-// Created by Vcvc on 2021/3/13.
-//
-
 #include "server.h"
+
+Server::Server()
+{
+    server_ip = "127.0.0.1";
+    server_port = 10236;
+    sql_db = "Webserver";
+    sql_name = "root";
+    sql_port = 3306;
+    sql_pwd = "Aa1248800211";
+
+}
+
+Server::~Server()
+{
+    mysql_close(&sql);     //关闭一个服务器连接。
+}
+
+void init_mysql()
+{
+
+}
--- server/server.h
+++ server/server.h
@@ -1,16 +1,32 @@
-//
-// Created by Vcvc on 2021/3/13.
-//
-
 #ifndef WEBSERVER_SERVER_H
 #define WEBSERVER_SERVER_H
 
+#include <mysql/mysql.h>
+#include <sys/socket.h>
+#ifdef __linux__
+#include <sys/epoll.h>
+#elif __APPLE__
+#include <sys/event.h>
+#include <sys/types.h>
+#endif
 
+class Server {
+public:
+    Server();
+    ~Server();
 
-class server {
+    void init_mysql();
 
+private:
+    Mysql* sql;
+
+    char* sql_name;
+    char* sql_db;
+    char* sql_pwd;
+    int sql_port;
+
+    int server_port;
+    char* server_ip;
 };
-
-
 
 #endif //WEBSERVER_SERVER_H
