b50f773ff8dd7235382085b16fbf00be95e05a14
Vcvc <vcvckw@gmail.com>
2021-03-14 22:00:22 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,62 +1,63 @@
-#include <iostream>
-//#include <getopt.h>
+#include <getopt.h>
 
-#include "server.h"
+#include "server/server.h"
 
-/*
- * static struct option server_c[] = {
+static struct option server_c[] = {
 
-        {"device", required_argument, NULL, 'd'},
-        {"savefile", optional_argument, NULL, 'f'},
-        {"help", no_argument, NULL, 'h'},
-        {"version", no_argument, NULL, 'v'},
-        {"Device list", no_argument, NULL, 'l'},
+        {"port", optional_argument, NULL, 'p'},
+        {"ip", optional_argument, NULL, 'l'},
+        {"user_name", required_argument, NULL, 'u'},
+        {"password", required_argument, NULL, 'w'},
+        {"database", required_argument, NULL, 'd'},
+        {"sql port", optional_argument, NULL, 'm'},
         {0, 0, 0, 0}
 };
-*/
 
 int main(int argc, char** argv)
 {
     int port = 10236;
-    char* ip = "127.0.0.1";
+    char* ip = nullptr;
+    char* sql_user_name;
+    char* sql_pwd;
+    char* sql_db;
+    int sql_port = 3306;
 
-    /*
-     * int opt = 0;
+    int opt = 0;
     int optionIndex = 0;
-    while((opt = getopt_long(argc, argv, "d:f:hvl", server_c, &optionIndex)) != -1)
+    while((opt = getopt_long(argc, argv, "p:l:u:w:d:m:", server_c, &optionIndex)) != -1)
     {
         switch(opt)
         {
-            case 'd':
-                device = optarg;
+            case 'p':
+                port = atoi(optarg);
                 break;
-            case 'f':
-                savefile = optarg;
-                break;
-            case 'h':
-                Vc::PcapUsage();
-                exit(1);
-            case 'v':
-                Vc::PcapVersion();
-                exit(1);
             case 'l':
-                VcDevice_List();
-                exit(1);
+                 ip = optarg;
+                break;
+            case 'u':
+                sql_user_name = optarg;
+                break;
+            case 'w':
+                sql_pwd = optarg;
+                break;
+            case 'd':
+                sql_db = optarg;
+                break;
+            case 'm':
+                sql_port = atoi(optarg);
+                break;
             default:
-                Vc::PcapUsage();
-                exit(1);
+                std::cout << "-h help" << std::endl;
+                exit(0);
         }
     }
-    */
 
-    if(argc > 2)
-    {
-        ip = argv[1];
-        port = atoi(argv[2]);
-    }
 
-    Server* user = new Server();
-    user->init_mysql();
+    SqlPool sql_pool;
+    Server server;
+    server.init(ip, port, sql_user_name, sql_pwd, sql_db, sql_port);
+    server.log_write();
+    server.init_mysql();
 
     std::cout << "操作完成" << std::endl;
     return 0;
