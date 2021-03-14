a4ce78c2289e67ea8ddcbf8b898d0a70d28cbd6f
Vcvc <vcvckw@gmail.com>
2021-03-14 22:00:35 +0800

    log

--- log/log.cpp
+++ log/log.cpp
@@ -1,1 +1,32 @@
 #include "log.h"
+
+Log::Log()
+{}
+
+Log::~Log()
+{}
+
+void Log::setFilename(const char* filename)
+{
+    time_t t = time(NULL);
+    /*
+     * https://blog.csdn.net/linfengmove/article/details/104562569
+     */
+    struct tm *sys_tm = localtime(&t);
+    struct tm my_tm = *sys_tm;
+
+    char t_time[24] = {0};
+    snprintf(t_time, 24, "%d_%02d_%02d-%02d_%02d", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, my_tm.tm_hour, my_tm.tm_min);
+
+    snprintf(m_file_name, 255, "%s_%s_%s", "./logs/", t_time, filename);
+}
+
+void Log::init(const char *file_name, int log_buf_size, int split_lines, int max_queue_size)
+{
+    setFilename(file_name);
+    if(max_queue_size > 1)
+    {
+        m_is_async = true;
+    }
+
+}
--- log/log.h
+++ log/log.h
@@ -1,12 +1,33 @@
 #ifndef WEBSERVER_LOG_H
 #define WEBSERVER_LOG_H
 
+#include <cstring>
+#include <stdio.h>
+#include <iostream>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <chrono>
+
 class Log {
 public:
+    static Log *get_instance()
+    {
+        static Log instance;
+        return &instance;
+    }
+
+    void setFilename(const char* filename);
+    void init(const char *file_name, int log_buf_size, int split_lines, int max_queue_size);
+
+private:
     Log();
     virtual ~Log();
 
-private:
+    char dir_name[128];
+    char m_file_name[128];
+    int log_line;
+    bool m_is_async;
 
 };
 
