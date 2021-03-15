12379aeeead4ce1bf9bc657948ad57b6850f818f
Vcvc <vcvckw@gmail.com>
2021-03-15 19:59:54 +0800

    log

--- log/log.cpp
+++ log/log.cpp
@@ -8,6 +8,7 @@ Log::~Log()
 
 void Log::setFilename(const char* filename)
 {
+    memset(m_file_name, '\0', sizeof(m_file_name));
     time_t t = time(NULL);
     /*
      * https://blog.csdn.net/linfengmove/article/details/104562569
@@ -16,17 +17,35 @@ void Log::setFilename(const char* filename)
     struct tm my_tm = *sys_tm;
 
     char t_time[24] = {0};
-    snprintf(t_time, 24, "%d_%02d_%02d-%02d_%02d", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, my_tm.tm_hour, my_tm.tm_min);
+    snprintf(t_time, 24, "%d_%02d_%02d-%02d:%02d", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday, my_tm.tm_hour, my_tm.tm_min);
 
-    snprintf(m_file_name, 255, "%s_%s_%s", "./logs/", t_time, filename);
+    snprintf(m_file_name, 255, "%s%s_%s", "./logs/", t_time, filename);
+
 }
 
 void Log::init(const char *file_name, int log_buf_size, int split_lines, int max_queue_size)
 {
-    setFilename(file_name);
+    m_log_buf_size = log_buf_size;
+    m_split_lines = split_lines;
+    m_buf = new char[m_log_buf_size];
+    memset(&m_buf, '\0', m_log_buf_size);
+
     if(max_queue_size > 1)
     {
         m_is_async = true;
+
     }
 
+    setFilename(file_name);
+
+    infile.open(m_file_name, std::ios::out);
+    if(!infile)
+    {
+        std::cout << "infile error" << std::endl;
+        exit(-1);
+    }
+
+    infile.close();
+
+    return true;
 }
--- log/log.h
+++ log/log.h
@@ -4,14 +4,15 @@
 #include <cstring>
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <pthread.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <stdarg.h>
 #include <chrono>
 
 class Log {
 public:
-    static Log *get_instance()
+    static Log* get_instance()
     {
         static Log instance;
         return &instance;
@@ -19,15 +20,20 @@ public:
 
     void setFilename(const char* filename);
     void init(const char *file_name, int log_buf_size, int split_lines, int max_queue_size);
+    std::ofstream infile;
 
 private:
     Log();
     virtual ~Log();
 
-    char dir_name[128];
+    char* m_buf; //缓冲区
     char m_file_name[128];
-    int log_line;
     bool m_is_async;
+    int m_split_lines;  //日志最大行数
+    int m_log_buf_size; //日志缓冲区大小
+    long long m_count;  //日志行数记录
+    int m_today;        //因为按天分类,记录当前时间是那一天
+    FILE *m_fp;         //打开log的文件指针
 
 };
 
